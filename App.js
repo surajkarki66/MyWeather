062500b5d95a0e6990b75de6e11bd7829ad661aa
Suraj Karki <suraj.karki500@gmail.com>
2020-12-11 03:10:49 +0545

    Added location permission.

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  useEffect(() => {
+    load();
+  }, []);
+  async function load() {
+    setErrorMessage(null);
+    try {
+      let { status } = await Location.requestPermissionsAsync();
+
+      if (status !== "granted") {
+        setErrorMessage("Access to location is needed to run the app");
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+      alert(`Longitude: ${longitude} and Latitude: ${latitude}`);
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
